2020bdf530669a1cffcf234826516d1c03daa58c
Dhanesh <dhanesh.budhrani@gmail.com>
2019-09-01 13:48:24 +0200

    add http to url if not present

--- request.py
+++ request.py
@@ -3,6 +3,8 @@ import requests
 def _get(*args, **kwargs):
   url = kwargs.get('url') 
   assert url
+  if not (url.startswith('http://') or url.startswith('https://')):
+    url = 'http://' + url
   response = requests.get(url)
   content = response.content
   print(content)
